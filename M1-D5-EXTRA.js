10f0898a2dc22c58ee64c546909cea4533473fa5
Matt Ginter <mattginter@gmail.com>
2021-12-05 23:34:32 +0100

    Fixed Issues in Ex2+

--- M1-D5-EXTRA.js
+++ M1-D5-EXTRA.js
@@ -44,14 +44,21 @@ console.log("\nEx. 2\n")
 
 const shoppingCart = [
     {name: "item1", price: 2, id: 0001, toShip: 7},
-    {name: "item1", price: 4, id: 0002, toShip: 5},
-    {name: "item1", price: 3, id: 0003, toShip: 12},
-    {name: "item1", price: 8, id: 0004, toShip: 2},
+    {name: "item2", price: 4, id: 0002, toShip: 5},
+    {name: "item3", price: 3, id: 0003, toShip: 12},
+    {name: "item4", price: 8, id: 0004, toShip: 2},
 ]
 
-const shoppingCartTotal = () => {
-
+const shoppingCartTotal = (shoppingCart) => {
+    let total = 0
+    for (let i = 0; i <= shoppingCart.length - 1; i++){
+        let itemTotal = shoppingCart[i].price * shoppingCart[i].toShip
+        total += itemTotal
+    }
+    return total
 }
+
+console.log(shoppingCartTotal(shoppingCart))
 
 /* EXTRA 3
  In your eCommerce you have an array of objects called shoppingCart. Each one of these objects has a price, a name, an id and the quantity to be shipped.
@@ -59,10 +66,16 @@ const shoppingCartTotal = () => {
 */
 console.log("\nEx. 3\n")
 
-const addToShoppingCart = () => {
-
+const newItem = {
+    name: "item5", price: 5, id: 0005, toShip: 5
 }
 
+const addItemReturnTotal = (newItem) => { //take object 
+    let newCart = shoppingCart.push(newItem) // new array + object
+    return newCart //return length of array
+}
+
+console.log(addItemReturnTotal(newItem))
 /* EXTRA 4
  In your eCommerce you have an array of objects called shoppingCart. Each one of these objects has a price, a name, an id and the quantity to be shipped.
  Create a function called "maxShoppingCart" which receives the shoppingCart array and returns the most expensive item in it.
@@ -77,7 +90,7 @@ const maxShoppingCart = () => {
  In your eCommerce you have an array of objects called shoppingCart. Each one of these objects has a price, a name, an id and the quantity to be shipped.
  Create a function called "latestShoppingCart" which receives the shoppingCart array and returns the last item.
 */
-console.log("\nEx. 5\n")
+// console.log("\nEx. 5\n")
 
 const latestShoppingCart = []
 
@@ -85,21 +98,21 @@ const latestShoppingCart = []
  Create a function called "loopUntil" which receives an integer x between 0 and 9 as a parameter.
  The function loops and prints a random number between 0 and 9 until the random number is bigger than x for three times in a row.
 */
-console.log("\nEx. 6\n")
+// console.log("\nEx. 6\n")
 
 const loopUntil = () => {}
 
 /* EXTRA 7
  Write a function called "average" which receives an array and returns the average numerical value. The function automatically skips non-numeric entries in the array.
 */
-console.log("\nEx. 7\n")
+// console.log("\nEx. 7\n")
 
 const average = () => {}
 
 /* EXTRA 8
  Write a function called "longest" to find the longest string from a given array of strings.
 */
-console.log("\nEx. 8\n")
+// console.log("\nEx. 8\n")
 
 const longest = () => {}
 
@@ -107,14 +120,14 @@ const longest = () => {}
  Write a function to create a very simple anti spam filter for your mailbox. The function takes a string emailContent, and returns a boolean.
  The function should return true if the emailContent string does not contain the words SPAM or SCAM.
 */
-console.log("\nEx. 9\n")
+// console.log("\nEx. 9\n")
 
 const spamFilter = () => {}
 
 /* EXTRA 10
  Write a function that receives a date as a parameter and calculates the number of days passed since the given date.
 */
-console.log("\nEx. 10\n")
+// console.log("\nEx. 10\n")
 
 const daysSince = () => {}
 
@@ -125,7 +138,7 @@ const daysSince = () => {}
  ["00","01","02"
  "10","11","12"]
 */
-console.log("\nEx. 11\n")
+// console.log("\nEx. 11\n")
 
 const matrixGenerator = () => {}
 
